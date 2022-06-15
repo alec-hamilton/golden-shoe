17a69d97cad599eacdb6c43a9cfe7c8fdecb9bda
alech <hamiltonalec@gmail.com>
2022-06-15 14:15:36 +0100

    db prepared

--- backend/repositories/shoesRepository.js
+++ backend/repositories/shoesRepository.js
@@ -12,8 +12,13 @@ const getShoes = async () => {
 const getShoe = async (id) => {
     console.log('Repository: getShoe ' + id);
 
-    let sqlQuery = 'SELECT `title`, `price`, `image` FROM products WHERE `id` = '
-                    + `${id}` + ';';
+    const sqlQuery = 'SELECT `brands`.`brand`, `products`.`model`, `products`.`price`, `products`.`image_name`, `products`.`stock`'
+                    + 'FROM `products`'
+                    + 'INNER JOIN `brands`'
+                    + 'ON `products`.`brand` = `brands`.`id`'
+                    + 'WHERE `products`.`id` = '
+                    + `${id}`
+                    + ';';
 
     return await shoesDbConnection.query(sqlQuery);
 }
--- backend/services/dbService.js
+++ backend/services/dbService.js
@@ -4,7 +4,7 @@ const connectToDb = async () => {
     const db = await mysql.createConnection({
         user: 'root',
         password: 'password',
-        database: 'robot_stores',
+        database: 'golden-shoe',
     });
     console.log('MySQL db connected');
     return db;
