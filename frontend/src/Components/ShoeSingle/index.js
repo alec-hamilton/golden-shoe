89216c232f99a97a0b37beb4a1581fdc4ac9ab9b
alech <hamiltonalec@gmail.com>
2022-06-18 18:30:39 +0100

    added new fetch for single shoe from be

--- frontend/src/Components/ShoeSingle/index.js
+++ frontend/src/Components/ShoeSingle/index.js
@@ -2,50 +2,47 @@ import {useParams} from "react-router-dom";
 import OrderButton from "../OrderButton";
 import {useEffect, useState} from "react";
 
-const ShoeSingle = ({shoeItems}) => {
+const ShoeSingle = () => {
 
     let {productId} = useParams();
     productId = productId ? parseInt(productId) : -1;
 
-    // const [shoeItems, setShoeItems] = useState([]);
-    //
-    // const fetchData = async () => {
-    //     const response = await fetch('http://localhost:3001/shoes');
-    //
-    //     if (!response.ok) {
-    //         throw new Error('Data could not be fetched.');
-    //     }
-    //
-    //     return await response.json();
-    // }
-    //
-    // useEffect(() => {
-    //         fetchData()
-    //             .then((shoeData) => {
-    //                 setShoeItems(shoeData);
-    //                 console.log(shoeItems[0].image_name);
-    //             })
-    //             .catch((e) => {
-    //                 console.log(e.message);
-    //             })
-    //     }, []
-    // );
+    const [shoeItem, setShoeItem] = useState([]);
 
+    const fetchData = async () => {
+        const response = await fetch('http://localhost:3001/shoes/' + productId);
 
+        if (!response.ok) {
+            throw new Error('Data could not be fetched.');
+        }
+        return await response.json();
+    }
+
+    useEffect(() => {
+            fetchData()
+                .then((shoeData) => {
+                    setShoeItem(shoeData);
+                    console.log(shoeItem);
+                })
+                .catch((e) => {
+                    console.log(e.message);
+                })
+        }, []
+    );
 
     return (
         <>
         <div>
-            <img src={`../../../images/${shoeItems[productId].image_name}`} alt="" />
+            <img src={`../../../images/${shoeItem[0].image_name}`} alt="" />
         </div>
         <div className="shoe-brand">
-            <h5 className="shoe-title">{shoeItems.brand}</h5>
+            <h5 className="shoe-title">{shoeItem[0].brand}</h5>
         </div>
         <ul className="model-price-list">
-            <li>{shoeItems.model}</li>
-            <li>£{shoeItems.price}</li>
+            <li>{shoeItem[0].model}</li>
+            <li>£{shoeItem[0].price}</li>
         </ul>
-        <OrderButton shoeItems={shoeItems}/>
+        <OrderButton shoeItem={shoeItem}/>
         <h1>{productId}</h1>
         </>
     );
